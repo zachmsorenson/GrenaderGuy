f7c31f9c15244726b09294d291bbb4fc76231696
zsore7 <sore1324@stthomas.edu>
2018-05-10 17:46:17 -0500

    cleaning up the server

--- server/server.js
+++ server/server.js
@@ -26,7 +26,7 @@ function init() {
     
     // Game loop
     setInterval(gameLoop, 200);
-};
+}
 
 function setEventHandlers() {
     io.on('connection', function(client){
@@ -35,21 +35,13 @@ function setEventHandlers() {
         client.on('newplayer', onNewPlayer);
         client.on('click', click);
         client.on('disconnect', onDisconnect);
-
+        client.on('start game', startGame);
+        client.on('place bomb', onPlaceBomb);
   //      client.on('join room', Lobby.onJoinRoom);
-        client.on('join room', Lobby.onJoinRoom);
-        client.on('right', moveRight);
-        client.on('left', moveLeft);
-        client.on('down', moveDown);
-        client.on('up', moveUp);
     });
-};
+}
+
 function onNewPlayer() {
-	this.player = {
-		id: server.lastPlayerID++,
-		x: randomInt(100, 400),
-		y: randomInt(100, 400)
-	}
 	this.emit('allplayers', getAllPlayers());
 	this.broadcast.emit('newplayer', this.player);
 }
@@ -67,22 +59,38 @@ function onNewPlayer(){
     console.log(game.players);
     this.emit('allplayers', getAllPlayers());
     this.broadcast.emit('newplayer', this.player);
-function moveRight (data) {
-    this.player.x = data.x + 20;
-    io.emit('move_direction', this.player);
+}
 
-}
-function moveLeft (data) {
-	this.player.x = data.x - 20;
-	io.emit('move_direction', this.player);
-}
-function moveDown (data) {
-	this.player.y = data.y - 20;
-	io.emit('move_direction', this.player);
-}
-function moveUp (data) {
-	this.player.y = data.y + 20;
-	io.emit('move_direction', this.player);
+function startGame(){
+    console.log('recieved start game message');
+
+    var i = 0;
+    Object.keys(io.sockets.connected).forEach(function(socketID){
+        var socket = io.sockets.connected[socketID];
+        if (lastPlayerID == 0 || lastPlayerID == 2){
+            var xSpawn = 1;
+        } else {
+            var xSpawn = 400;
+        }
+
+        if (lastPlayerID == 0 || lastPlayerID == 1){
+            var ySpawn = 1;
+        } else {
+            var ySpawn = 400;
+        }
+        
+        socket.player = {
+            id: lastPlayerID++,
+            x: xSpawn,
+            y: ySpawn,
+            alive: true,
+            hasMoved: false
+        }
+    });
+    io.sockets.emit('start game');
+    setTimeout(function(){
+        io.sockets.emit('allplayers', getAllPlayers());
+    }, 500);
 }
 
 function click(data){
@@ -97,16 +105,23 @@ function click(data){
 }
 
 function onDisconnect(){
+    console.log(io.sockets.connected.length);
+    if (Object.keys(io.sockets.connected).length === 0){
+        // game is empty restart i
+        console.log("empty");
+        game = new Game();
+        lastPlayerID = 0;
+    }
     if (this.player){
         io.emit('remove', this.player.id);
     }
 }
 
-function onDropBomb(data){
+function onPlaceBomb(data){
     if (this.player){
         var player = this.player;
         var bomb = new Bomb(player.x, player.y, lastBombID);
-        io.emit('drop bomb', bomb);
+        io.emit('place bomb', bomb);
     }
 }
 
@@ -130,16 +145,12 @@ function randomInt (low, high) {
 }
 
 function gameLoop() {
-    // on each loop, check if a player has moved.
-    // if they have, emit that move
-//    for(var game in games){
-//        var game = games[game];
-        for(var i in game.players) {
-            var player = game.players[i]; // for each player
-            if(player.alive && player.hasMoved) {
-                io.emit('move', player);
-                player.hasMoved = false;
-            }
+    for(var i in game.players) {
+        var player = game.players[i]; // for each player
+        if(player.alive && player.hasMoved) {
+            io.emit('move', player);
+            player.hasMoved = false;
         }
-//    }
-}    
+    }
+} 
+
