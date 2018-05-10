ad654efd77fcd26df8c7d5d9e0a697cea422ccb8
zsore7 <sore1324@stthomas.edu>
2018-05-10 15:16:38 -0500

    Incorporating tilemap

--- client/js/game.js
+++ client/js/game.js
@@ -5,27 +5,37 @@ Game.init = function(){
 };
 
 Game.preload = function(){
-    game.load.tilemap('map', 'assets/map/example_map.json', null, Phaser.Tilemap.TILED_JSON);
-    game.load.spritesheet('tileset', 'assets/map/tilesheet.png', 32, 32);
+    game.load.tilemap('map', 'assets/map/secondTilemap.json', null, Phaser.Tilemap.TILED_JSON);
+    game.load.spritesheet('tileset', 'assets/map/simples_pimples.png', 32, 32);
     game.load.image('sprite', 'assets/sprites/sprite.png');
 };
 
 Game.create = function(){
     Game.playerMap = {};
+
+    // add tilemap to game
     var map = game.add.tilemap('map');
-    map.addTilesetImage('tilesheet', 'tileset'); // tilesheet is the key of the tileset in map's JSON file
-    var layer;
-    for(var i = 0; i < map.layers.length; i++) { // create each layer specified by map
-        layer = map.createLayer(i);
-    }
-    layer.inputEnabled = true; // Allows clicking on map
+    // add the tileset ontop of the tilemap
+    map.addTilesetImage('opensTileset', 'tileset');
+
+    // set up layers for tilemap
+    var backgroundLayer = map.createLayer('backgroundLayer');
+    var blockedLayer = map.createLayer('blockedLayer');
+    var objectLayer = map.createLayer('objectLayer');
+
+    map.setCollisionBetween(1, 5000, true, blockedLayer);
+    map.setCollisionBetween(1, 5000, true, objectLayer);
+    console.log(blockedLayer);
+    backgroundLayer.inputEnabled = true; // Allows clicking on map
     Client.askNewPlayer();
 
-    layer.events.onInputUp.add(Game.getCoordinates, this);
+    backgroundLayer.events.onInputUp.add(Game.getCoordinates, this);
 };
 
 Game.addNewPlayer = function(id, x, y){ // add a new player to the game map
-    Game.playerMap[id] = game.add.sprite(x, y, 'sprite'); // the player in the player map id with
+     // the player in the player map id with
+    var sprite = Game.playerMap[id] = game.add.sprite(x, y, 'sprite'); 
+    game.physics.arcade.enable(sprite);
     // passed id is added to the game with a sprite
 };
 
